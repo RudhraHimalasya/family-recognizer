28b20bb48cb24223f0596e37479c3572e983cbea
RudhraHimalasya <79926737+RudhraHimalasya@users.noreply.github.com>
2022-05-26 20:07:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-Webcam.set({
-    width:350,
-    height:300,
-    image_format:'png',
-    png_quality:90
-});
-camera = document.getElementById("cam");
-Webcam.attach('#cam');
+Webcam.attach('#camera');
+ camera = document.getElementById("camera"); 
+ Webcam.set(
+   { width:350,
+     height:300, 
+     image_format : 'png'
+     , png_quality:90 })
+   ;
+
 
 function takesnapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
+
+  console.log()
+// classifier = ml5.imageClassifier('',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Tmv2eI59f/model.json', modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+  
